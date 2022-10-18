31919f3b3f105f776f3c5214d4816ecb1c8b0d8f
exenmensch3d <haugdavid@icloud.com>
2022-10-18 15:52:23 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,56 @@
+import React, { useState , useCallback} from 'react';
+import { View, Text, Button, StyleSheet, TextInput } from 'react-native';
+
+
+
+const App = () => {
+
+  return(
+  <View testID="welcome" style={styles.container}>
+      <GroceryShoppingList />
+  </View>
+  
+    );
+
+  
+  }
+ 
+function GroceryShoppingList() {
+    const [groceryItem, setGroceryItem] = useState('');
+    const [items, setItems] = useState([]);
+  
+    const addNewItemToShoppingList = useCallback(() => {
+      setItems([groceryItem, ...items]);
+      setGroceryItem('');
+    }, [groceryItem, items]);
+  
+    return (
+      <>
+        <TextInput testID='textFeld'
+          value={groceryItem}
+          placeholder="Enter grocery item"
+          onChangeText={(text) => setGroceryItem(text)}
+        />
+        <Button
+        testID='submitKnopf'
+          title="Add the item to list"
+          onPress={addNewItemToShoppingList}
+        />
+        {items.map((item) => (
+          <Text testID='ekListe' key={item}>{item}</Text>
+        ))}
+      </>
+    );
+  }
+  
+
+// React Native Styles
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+
+export default App;
---
+++ e2e/firstTest.e2e.js
@@ -0,0 +1,28 @@
+describe('Example', () => {
+  beforeAll(async () => {
+    await device.launchApp();
+  });
+
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it('should have welcome screen', async () => {
+    await expect(element(by.id('welcome'))).toBeVisible();
+  });
+
+  it('should add item to shopping list', async () => {
+    await element(by.id('textFeld')).typeText("Bananas");
+    await element(by.id('submitKnopf')).tap();
+    await element(by.id('textFeld')).typeText("Milk");
+    await element(by.id('submitKnopf')).tap();
+    await element(by.id('textFeld')).typeText("Icetea");
+    await element(by.id('submitKnopf')).tap();
+    await element(by.id('textFeld')).typeText("Beer");
+    await element(by.id('submitKnopf')).tap();
+
+
+    await expect(element(by.text('Bananas'))).toBeVisible();
+    await expect(element(by.text('Beer'))).toBeVisible();
+  });
+});
